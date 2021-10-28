634c7571dbdc4604bc7dcd15d2f4d3ac2da068ee
kadiace <rlacjfghks95@gmail.com>
2021-10-29 02:10:15 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+var db_config = require(__dirname + '/config/database.js');
+var conn = db_config.init();
+var bodyParser = require('body-parser');
+
+db_config.connect(conn);
